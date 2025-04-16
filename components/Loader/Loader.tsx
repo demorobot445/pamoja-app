40bdc2e51de4e6cd35f69e414d5a4d89adf533c1
Ahmed Khan <demo11website@gmail.com>
2025-04-16 10:01:38 +0500

    add gif loader

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -5,7 +5,7 @@ const Loader = () => {
   const { progress } = useProgress();
 
   return (
-    <Html position={[0, 0.5, 0]} fullscreen>
+    <Html position={[0, 0, 0]} fullscreen>
       <Image
         className={`object-cover transition-all duration-1000 delay-[1.2s] ${
           progress === 100 ? "opacity-0" : "opacity-100"
--- components/Render/Render.tsx
+++ components/Render/Render.tsx
@@ -1,10 +1,11 @@
-import { Environment, Loader } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useRef } from "react";
 import { Color } from "three";
 import AnimatedCamera from "../AnimatedCamera/AnimatedCamera";
 import { Building } from "../Models/Building";
 import Instruction from "../Header/Instruction";
+import Loader from "../Loader/Loader";
 
 type Props = {
   frameTl: React.RefObject<gsap.core.Timeline | null>;
@@ -17,15 +18,14 @@ const Render: React.FC<Props> = ({ frameTl }) => {
     <div ref={containerRef}>
       <div id="react-three-canvas" className="h-lvh fixed inset-0">
         <Canvas shadows={"soft"}>
-          <Instruction />
-          <AnimatedCamera containerRef={containerRef} />
-          <Environment preset="warehouse" />
-          <color args={[new Color("gray")]} attach={"background"} />
-          <Suspense fallback={null}>
+          <Suspense fallback={<Loader />}>
+            <Instruction />
+            <AnimatedCamera containerRef={containerRef} />
+            <Environment preset="warehouse" />
+            <color args={[new Color("gray")]} attach={"background"} />
             <Building frameTl={frameTl} />
           </Suspense>
         </Canvas>
-        <Loader />
       </div>
     </div>
   );
